355385472826beaeb3bcca94d9fc951edcd7ff7e
Shooq <shooqjaboob@gmail.com>
2025-02-24 13:46:05 +0400

    front-end

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,10 +1,30 @@
-
+import logo from "../Images/logo-t.png";
+import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
 const Header = () => {
-
   return (
-    <h1>Header</h1>
+    <>
+      <Navbar className="header">
+        <Nav>
+          <NavItem>
+            <img src={logo} className="logo-t" />
+          </NavItem>
+          <NavItem className="nav-link a">
+            <Link to="/">Home</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/login">login</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/profile">Profile</Link>
+          </NavItem>
 
-
+          <NavItem>
+            <Link to="/register">Logout</Link>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </>
   );
 };
 
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,9 +1,51 @@
 import LoginImages from "../Images/loginImage.jpg";
+import { Container, Row, Col, Form, Button } from "reactstrap"; //import the Reactstrap Components
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div>
       <h1>Login</h1>
-      <img src={LoginImages} />
+
+      <Container>
+        <Form>
+          <Row>
+            <Col md={3}>
+              Email
+              <input
+                id="exampleText"
+                name="text"
+                placeholder="Enter Password"
+                type="textarea"
+              />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}>
+              Password
+              <input
+                id="exampleText"
+                name="text"
+                placeholder="Enter Password"
+                type="textarea"
+              />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}></Col>
+          </Row>
+
+          <Row>
+            <Col md={3}></Col>
+          </Row>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+      <p className="smalltext">
+        No Account? <Link to="/register">Sign Up now.</Link>
+      </p>
     </div>
   );
 };
