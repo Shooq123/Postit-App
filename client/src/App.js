9974fa627ea540026f9772581bd16cc6256f2f81
Your Name <you@example.com>
2025-03-17 00:12:00 +0400

    add updateuser reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "reactstrap";
-import Footer from "./Components/Footer";
+import { Container, Row, Col } from "reactstrap"; //import the Reactstrap Components
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
 import Register from "./Components/Register";
+import UpdateUser from "./Components/UpdateUser";
+
 const App = () => {
   return (
     <Container fluid>
@@ -19,12 +21,11 @@ const App = () => {
         <Row className="main">
           <Routes>
             <Route path="/" element={<Home />}></Route>
-
             <Route path="/login" element={<Login />}></Route>
-
             <Route path="/profile" element={<Profile />}></Route>
-
             <Route path="/register" element={<Register />}></Route>
+            <Route path="/update" element={<UpdateUser />}></Route>
+            <Route path="/update/:email" element={<UpdateUser />}></Route>
           </Routes>
         </Row>
 
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,62 +1,75 @@
-import LoginImages from "../Images/loginImage.jpg";
-import { Container, Row, Col, Form, Button } from "reactstrap"; //import the Reactstrap Components
+
+import loginImage from "../Images/loginImage.jpg";
+import {
+  Button,
+  Col,
+  Label,
+  Container,
+  Row,
+  FormGroup,
+  Input,
+  Form,
+} from "reactstrap";
+import logo from "../Images/logo-t.png";
 import { Link } from "react-router-dom";
-import { userSchema } from "../Validations/UserValidations";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { userSchema} from "../Validations/UserValidations";
+
 const Login = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: yupResolver(userSchema) });
+
+  var x = 1;
+  // Handle form submission
+
   const onSubmit = (data) => {
-    console.log("Form Data", data);
+    console.log("Form Data", data); // You can handle the form submission here
   };
   return (
     <div>
-      <h1>Login</h1>
-
       <Container>
+        <img src={logo} />
         <Form onSubmit={handleSubmit(onSubmit)}>
           <Row>
-            <Col md={3}>
-              <p>Email:</p>
-              <input
-                id="exampleText"
-                name="text"
-                placeholder="Enter Password"
-                type="textarea"
-                {...register("email")}
-              />
+            <Col md={5}>
+              Username<br></br>
+              <input type="email" name="email" {...register("email")}></input>
             </Col>
             <p className="error">{errors.email?.message}</p>
           </Row>
 
           <Row>
-            <Col md={3}>
-              <p>Password:</p>
+            <Col md={5}>
+              Password<br></br>
               <input
-                id="exampleText"
-                name="text"
-                placeholder="Enter Password"
-                type="textarea"
+                type="password"
+                name="password"
                 {...register("password")}
-              />
+              ></input>
             </Col>
-            <p className="error">{errors.password?.message}</p>
+            <p className="error">{errors.email?.message}</p>
           </Row>
 
           <Row>
-            <Col md={3}></Col>
+            <Col md={5}>
+              <Button>Login</Button>
+            </Col>
           </Row>
-          <Button>Submit</Button>
+
+          <Row>
+            <Col md={6}>
+              <p className="smalltext">
+                No Account? <Link to="/register">Sign Up now.</Link>
+              </p>
+            </Col>
+          </Row>
         </Form>
       </Container>
-      <p className="smalltext">
-        No Account? <Link to="/register">Sign Up now.</Link>
-      </p>
     </div>
   );
 };
